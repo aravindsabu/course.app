463f0d1309b101c2c1ce0b79c2f704a9f6671bfc
aravindsabu <aravindsabu2001@gmail.com>
2022-10-10 17:57:26 +0530

    ok

--- src/app/addcourse/addcourse.component.ts
+++ src/app/addcourse/addcourse.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AddcourseComponent implements OnInit {
 
   constructor() { }
+ 
 
   ngOnInit(): void {
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddcourseComponent } from './addcourse/addcourse.component';
 import { Router, RouterModule, Routes } from '@angular/router';
+import { ViewallComponent } from './viewall/viewall.component';
 
 const appRoutes:Routes = [
   {
     path:"",component:AddcourseComponent
+  },
+  {
+    path:"view",component:ViewallComponent
   }
 ]
 
@@ -17,7 +21,8 @@ const appRoutes:Routes = [
   declarations: [
     AppComponent,
     NavbarComponent,
-    AddcourseComponent
+    AddcourseComponent,
+    ViewallComponent
   ],
   imports: [
     BrowserModule,
